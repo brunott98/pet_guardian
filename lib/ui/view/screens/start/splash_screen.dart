6a0a96fb2ba11fb068bae0ce8574e5cc344ee53d
Bruno <childe.teixeira@gmail.com>
2024-02-17 12:10:43 -0300

    Themes correction

--- lib/ui/view/screens/start/splash_screen.dart
+++ lib/ui/view/screens/start/splash_screen.dart
@@ -13,19 +13,7 @@ class PetGuardianApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Pet Guardian',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-
-
-        primaryColor: AppColors.primaryColor,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-
-
-              ),
-        ),
-      ),
+      theme: AppThemes.lightTheme(),
       home: const SplashScreen(),
     );
   }
